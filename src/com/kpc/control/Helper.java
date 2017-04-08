d8e0d8c439620bae96e1c31ea1f54d6234681e37
cgw <cgw@cgw-PC.lan>
2017-04-08 23:47:44 +0800

    Change-Id: I3a7dbacec827ec6678a440444ed5e58ee97ddc8a Signed-off-by: cgw <cgw@cgw-PC.lan>

--- src/com/kpc/control/Helper.java
+++ src/com/kpc/control/Helper.java
@@ -21,7 +21,6 @@ import com.kpc.dao.FoodDTO;
 public class Helper extends HttpServlet {
     
     KpcOrder kpc = new KpcOrder();
-    // 11
 
     /* 璁㈤鍙�夐鐗╂�绘暟 */
     private final Integer count = 10;
